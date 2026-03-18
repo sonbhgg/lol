c3a5e834f9548249de3c48e7a77e73f1a2185a56
Veronica <akim60452@gmail.com>
2026-03-18 10:07:49 +0300

    Реализована форма регистрации

---
+++ action.php
@@ -0,0 +1,43 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    $name = isset($_POST['name']) ? trim($_POST['name']) : '';
+    
+    $email = isset($_POST['email']) ? trim($_POST['email']) : '';
+    
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
+    
+    $confirm_password = isset($_POST['confirm_password']) ? $_POST['confirm_password'] : '';
+    
+    $gender = isset($_POST['gender']) ? $_POST['gender'] : '';
+    
+    if (empty($email)) {
+        $errors['email'] = 'Поле Email обязательно для заполнения';
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = 'Введите корректный email адрес';
+    }
+    
+    if (empty($password)) {
+        $errors['password'] = 'Поле Пароль обязательно для заполнения';
+    } elseif (strlen($password) < 6) {
+        $errors['password'] = 'Пароль должен содержать минимум 6 символов';
+    }
+    
+    if (!empty($password) && $password !== $confirm_password) {
+        $errors['confirm_password'] = 'Пароли не совпадают';
+    }
+    
+    if (empty($gender)) {
+        $errors['gender'] = 'Выберите пол';
+    }
+    
+    if (!empty($errors)) {
+    	exit;
+    } else {
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        header('Location: facts.html');
+    }
+    
+} else {
+    exit;
+}
